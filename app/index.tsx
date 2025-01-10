316dbcf7986013d89512477dca01400302561f6e
vashista <vashista@lemonpay.tech>
2025-01-10 21:33:48 +0530

    commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,277 +1,114 @@
-// import {
-//   Text,
-//   StyleSheet,
-//   Alert,
-//   Button,
-//   TextInput,
-//   TouchableOpacity,
-//   KeyboardAvoidingView,
-//   Platform,
-// } from "react-native";
-// import React, { useState } from "react";
-// import Colors from "../constants/Colors";
-// import Spinner from "react-native-loading-spinner-overlay";
-// import { useAuth } from "../context/AuthContext";
-
-// const Page = () => {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [loading, setLoading] = useState(false);
-//   const { onLogin, onRegister } = useAuth();
-
-//   // Sign in with email and password
-//   const onSignInPress = async () => {
-//     setLoading(true);
-
-//     try {
-//       const result = await onLogin!(email, password);
-//     } catch (e) {
-//       Alert.alert("Error", "Could not log in");
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   // Create a new user
-//   const onSignUpPress = async () => {
-//     setLoading(true);
-//     try {
-//       const result = await onRegister!(email, password);
-//     } catch (e) {
-//       Alert.alert("Error", "Could not log in");
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   return (
-//     <KeyboardAvoidingView
-//       behavior={Platform.OS === "ios" ? "padding" : "height"}
-//       style={styles.container}
-//     >
-//       <Spinner visible={loading} />
-//       <Text style={styles.header}>Meet Me</Text>
-//       <Text style={styles.subheader}>The fastest way to meet</Text>
-//       <TextInput
-//         autoCapitalize="none"
-//         placeholder="john@doe.com"
-//         value={email}
-//         onChangeText={setEmail}
-//         style={styles.inputField}
-//       />
-//       <TextInput
-//         placeholder="password"
-//         value={password}
-//         onChangeText={setPassword}
-//         secureTextEntry
-//         style={styles.inputField}
-//       />
-
-//       <TouchableOpacity onPress={onSignInPress} style={styles.button}>
-//         <Text style={{ color: "#fff" }}>Sign in</Text>
-//       </TouchableOpacity>
-//       <Button
-//         onPress={onSignUpPress}
-//         title="Create Account"
-//         color={Colors.primary}
-//       ></Button>
-//     </KeyboardAvoidingView>
-//   );
-// };
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     padding: 20,
-//     paddingHorizontal: "20%",
-//     justifyContent: "center",
-//   },
-//   header: {
-//     fontSize: 30,
-//     textAlign: "center",
-//     marginBottom: 10,
-//   },
-//   subheader: {
-//     fontSize: 18,
-//     textAlign: "center",
-//     marginBottom: 40,
-//   },
-//   inputField: {
-//     marginVertical: 4,
-//     height: 50,
-//     borderWidth: 1,
-//     borderColor: Colors.primary,
-//     borderRadius: 4,
-//     padding: 10,
-//   },
-//   button: {
-//     marginVertical: 15,
-//     alignItems: "center",
-//     backgroundColor: Colors.primary,
-//     padding: 12,
-//     borderRadius: 4,
-//   },
-// });
-// export default Page;
-
 import {
-  View,
-  StyleSheet,
-  ScrollView,
   Text,
-  ImageBackground,
-  TouchableOpacity,
-  Dimensions,
+  StyleSheet,
   Alert,
+  Button,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
-//   import { rooms } from '../../assets/data/rooms';
-import { Link, useRouter } from "expo-router";
-const WIDTH = Dimensions.get("window").width;
-const HEIGHT = Dimensions.get("window").height;
-
-import { Ionicons } from "@expo/vector-icons";
-import Colors from "@/constants/Colors";
+import React, { useState } from "react";
+import Colors from "../constants/Colors";
+import Spinner from "react-native-loading-spinner-overlay";
+import { useAuth } from "../context/AuthContext";
 
 const Page = () => {
-  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { onLogin, onRegister } = useAuth();
 
-  // Create random id and navigate to the room
-  const onStartMeeting = async () => {
-    const randomId = Math.floor(Math.random() * 1000000000).toString();
-    router.push(`/(inside)/(room)/${randomId}`);
+  // Sign in with email and password
+  const onSignInPress = async () => {
+    setLoading(true);
+
+    try {
+      const result = await onLogin!(email, password);
+    } catch (e) {
+      Alert.alert("Error", "Could not log in");
+    } finally {
+      setLoading(false);
+    }
   };
 
-  // Prompt user to enter a call id and navigate to the room
-  const onJoinMeeting = () => {
-    console.log("Clicked on onJoinMeeting");
-    Alert.prompt(
-      "Join",
-      "Please enter your Call ID:",
-      (id) => {
-        console.log("Joining call: ", id);
-        router.push(`/(inside)/(room)/${id}`);
-      },
-      "plain-text"
-    );
+  // Create a new user
+  const onSignUpPress = async () => {
+    setLoading(true);
+    try {
+      const result = await onRegister!(email, password);
+    } catch (e) {
+      Alert.alert("Error", "Could not log in");
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <ScrollView
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={styles.container}
-      contentContainerStyle={{ paddingBottom: 20 }}
     >
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <TouchableOpacity onPress={onStartMeeting} style={styles.button}>
-          <Ionicons name="videocam-outline" size={24} />
-          <Text style={styles.buttonText}>Start new Meeting</Text>
-        </TouchableOpacity>
+      <Spinner visible={loading} />
+      <Text style={styles.header}>Meet Me</Text>
+      <Text style={styles.subheader}>The fastest way to meet</Text>
+      <TextInput
+        autoCapitalize="none"
+        placeholder="john@doe.com"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.inputField}
+      />
+      <TextInput
+        placeholder="password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={styles.inputField}
+      />
 
-        <TouchableOpacity onPress={onJoinMeeting} style={styles.button}>
-          <Ionicons name="business-outline" size={24} />
-          <Text style={styles.buttonText}>Join Meeting by ID</Text>
-        </TouchableOpacity>
-      </View>
-
-      {/* <View style={styles.divider}>
-        <View
-          style={{
-            flex: 1,
-            height: StyleSheet.hairlineWidth,
-            backgroundColor: "#000",
-          }}
-        />
-        <Text style={{ fontSize: 18 }}>or join public room</Text>
-        <View
-          style={{
-            flex: 1,
-            height: StyleSheet.hairlineWidth,
-            backgroundColor: "#000",
-          }}
-        />
-      </View> */}
-
-      {/* <View style={styles.wrapper}>
-          {rooms.map((room, index) => (
-            <Link href={`/(inside)/(room)/${room.id}`} key={index} asChild>
-              <TouchableOpacity>
-                <ImageBackground
-                  key={index}
-                  source={room.img}
-                  style={styles.image}
-                  imageStyle={{ borderRadius: 10 }}>
-                  <View style={styles.overlay}>
-                    <Text style={styles.text}>{room.name}</Text>
-                  </View>
-                </ImageBackground>
-              </TouchableOpacity>
-            </Link>
-          ))}
-        </View> */}
-    </ScrollView>
+      <TouchableOpacity onPress={onSignInPress} style={styles.button}>
+        <Text style={{ color: "#fff" }}>Sign in</Text>
+      </TouchableOpacity>
+      <Button
+        onPress={onSignUpPress}
+        title="Create Account"
+        color={Colors.primary}
+      ></Button>
+    </KeyboardAvoidingView>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-  },
-  wrapper: {
-    alignItems: "center",
+    padding: 20,
+    paddingHorizontal: "20%",
     justifyContent: "center",
-    flexDirection: WIDTH > HEIGHT ? "row" : "column",
-    gap: 20,
   },
-  button: {
-    flex: 1,
-    gap: 10,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: Colors.secondary,
-    margin: 20,
-    padding: 30,
-    borderRadius: 10,
+  header: {
+    fontSize: 30,
+    textAlign: "center",
+    marginBottom: 10,
   },
-  buttonText: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginRight: 10,
-  },
-  divider: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 10,
-    marginHorizontal: 20,
-    marginTop: 20,
+  subheader: {
+    fontSize: 18,
+    textAlign: "center",
     marginBottom: 40,
   },
-
-  image: {
-    width: WIDTH > HEIGHT ? WIDTH / 4 - 30 : WIDTH - 40,
-    height: 300,
+  inputField: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderColor: Colors.primary,
+    borderRadius: 4,
+    padding: 10,
   },
-  overlay: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: "center",
+  button: {
+    marginVertical: 15,
     alignItems: "center",
-    backgroundColor: "rgba(0,0,0,0.4)",
-    borderRadius: 10,
-  },
-  text: {
-    color: "#fff",
-    fontSize: 30,
-    fontWeight: "bold",
-    textAlign: "center",
+    backgroundColor: Colors.primary,
+    padding: 12,
+    borderRadius: 4,
   },
 });
-
 export default Page;
